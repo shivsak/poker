bca60a515af45df7c5776f3b90485d577220b191
Shiv Sakhuja <shivsak.s@gmail.com>
2017-02-26 08:22:44 -0500

    Heroku edits

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: python flask_app.py
--- app.py
+++ app.py
@@ -64,4 +64,6 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
